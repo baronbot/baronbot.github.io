0e16cbdce020a32d499d6a3748011e10f41ac2bf
Phyxsius <phyxsius21@hotmail.com>
2018-09-28 12:04:27 +0100

    Locale/language selector implementation.

--- script.js
+++ script.js
@@ -1,41 +1,101 @@
-$(document).ready(function() {
-    $('head').append('<link href="https://i.imgur.com/i5999xB.png" rel="shortcut icon" type="image/x-icon" />');
+$(document).ready(function () {
+    $("head").append("<link href='https://i.imgur.com/i5999xB.png' rel='shortcut icon' type='image/x-icon'/>");
 
-    $.getJSON("https://ptb.discordapp.com/api/guilds/264370940706553857/widget.json", function(result) {
+    setLanguage("en");
+    const ps = new PerfectScrollbar(".dropdown-content");
 
-        for(i=0; i < result.channels.length; i++){
-            if(result.channels[i].position === 0){
-                var serverNum = result.channels[i].name.replace(":", "");
-            } else if (result.channels[i].position === 1){
-                var usersNum = result.channels[i].name.replace(":", "");
-            } else if(result.channels[i].position === 2){
-                var channelsNum = result.channels[i].name.replace(":", "");
+    $.getJSON("https://api.jsonbin.io/b/5b8e8777db948c68635c7c81/latest", function (result) {
+        //Insert stats.
+        $(".statsServersNum").text(numberWithCommas(result.stats.s));
+        $(".statsUsersNum").text(numberWithCommas(result.stats.u));
+        $(".statsChannelsNum").text(numberWithCommas(result.stats.c));
+
+        console.log("[x] Stats set successfully.");
+
+        //Insert translators.
+        let uniqueTranslators = _.uniqBy(result.translators, "n");
+        let translatorsArray = _.sortBy(uniqueTranslators, "l");
+
+        let left = true;
+        for (i in translatorsArray) {
+            if (left) {
+                $(".translatorsList1").append("<div class='translatorsListItem'><img src='" + translatorsArray[i].a + "'><span>" + translatorsArray[i].n + "</span></div>")
+                left = false;
+            } else {
+                $(".translatorsList2").append("<div class='translatorsListItem'><img src='" + translatorsArray[i].a + "'><span>" + translatorsArray[i].n + "</span></div>")
+                left = true;
             }
         }
-
-        $('.statsServersNum').text(numberWithCommas(serverNum));
-        $('.statsUsersNum').text(numberWithCommas(usersNum));
-        $('.statsChannelsNum').text(numberWithCommas(channelsNum));
-
-        console.log("[x] Data success. ")
+        console.log("[x] Translators set successfully.");
     });
 
-    $('.topBar-GetBaron').on('click', function() {
-        window.open("https://discordapp.com/oauth2/authorize?&client_id=240482527695994880&scope=bot&permissions=52224", '_blank');
+    //Top bar "shortcuts".
+    $(".topBar-GetBaron").on("click", function () {
+        window.open("https://discordapp.com/oauth2/authorize?&client_id=240482527695994880&scope=bot&permissions=52224", "_blank");
     });
 
-    $(".topBar-Commands").click(function() {
-        $('html, body').animate({
+    $(".topBar-Commands").click(function () {
+        $("html, body").animate({
             scrollTop: $("#commands").offset().top
         }, 2000);
     });
 
-    $(".topBar-About").click(function() {
-        $('html, body').animate({
+    $(".topBar-About").click(function () {
+        $("html, body").animate({
             scrollTop: $("#about").offset().top
         }, 2000);
     });
+
+    //Language selector dropdown.
+    $(".dropdown-content, .dropbtn").on("mouseover", function () {
+        ps.update();
+
+        if ($(window).width() > 466) {
+            $(".dropbtn").css("background-color", "#303236");
+        }
+    });
+
+    $(".dropdown-content, .dropbtn").on("mouseout", function () {
+        if ($(window).width() > 466) {
+            $(".dropbtn").css("background-color", "inherit");
+        }
+    });
+
+    $(".dropdown-content a").on("click", function () {
+        let img = $(this).find("img").attr("src");
+        let lang = $(this).text();
+        let locale = $(this).attr("locale");
+
+        $(".dropbtn-img").attr("src", img);
+        $(".dropbtn-txt").text(lang);
+
+        setLanguage(locale);
+    });
+
+    //Translators Modal.
+    $(".trigger").click(function () {
+        $(".modal-wrapper").toggleClass("open");
+        $(".page-wrapper").toggleClass("blur");
+        $("body").css("overflow", "hidden");
+    });
+
+    $(".btn-close").click(function () {
+        $("body").css("overflow", "visible");
+    });
 });
+
+function setLanguage(locale) {
+    $.getJSON("locales/" + locale + ".json", function (json) {
+        let toTranslate = $("[translate]");
+        toTranslate.each(function () {
+            let elemAttr = $(this).attr("translate");
+            let translatedString = _.get(json, elemAttr);
+
+            $(this).html(translatedString);
+        });
+        console.log("[x] Language set: " + locale.toUpperCase() + ".");
+    });
+}
 
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
